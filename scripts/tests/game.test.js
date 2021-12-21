f767f128c840ba8efdc9a3b3dd127c521deb6b90
mwarddev <martinward1981@googlemail.com>
2021-12-21 13:34:49 +0000

    Add showTurns function and tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -28,6 +28,9 @@ describe("game object contains correct keys", () => {
     test("choices contains the correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+    test("turn number key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    })
 });
 
 describe("new game works correctly", () => {
@@ -72,5 +75,10 @@ describe("gameplay works correctly", () => {
         let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0]);
         expect(button.classList).toContain("light");
+    });
+    test("ShowTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
     });
 });
